0cedc2de4e369fdf4ab15b56a2fc0d63d5cd4059
Ahmad Shiddiq <ahmadshiddiqamin@gmail.com>
2022-03-28 00:21:47 +0700

    fix: grid

--- pages/backup.js
+++ pages/backup.js
@@ -1,4 +1,4 @@
-export default function backup() {
+export default function Backup() {
   return (
     <>
       <section className="bg-purple-wave bg-bottom bg-repeat-x ">
@@ -12,68 +12,68 @@ export default function backup() {
           </div>
         </nav> */}
 
-        <div className="flex items-center justify-center min-h-screen flex-col lg:flex-row">
+        <div className="flex min-h-screen flex-col items-center justify-center lg:flex-row">
           <div className="flex flex-col items-center gap-[60px]">
-            <img src="circle.svg" alt="" className="w-[30px] h-[30px]" />
+            <img src="circle.svg" alt="" className="h-[30px] w-[30px]" />
             <div className="flex w-[147px] justify-between">
-              <img src="cross.svg" alt="" className="w-[30px] h-[30px]" />
-              <img src="kite.svg" alt="" className="w-[30px] h-[30px]" />
+              <img src="cross.svg" alt="" className="h-[30px] w-[30px]" />
+              <img src="kite.svg" alt="" className="h-[30px] w-[30px]" />
             </div>
           </div>
-          <h1 className="text-[30px] font-medium leading-[120%] text-[#432261] text-center px-[59px] mt-[37px] mb-[37px] lg:w-[870px] lg:text-[64px]">
+          <h1 className="mt-[37px] mb-[37px] px-[59px] text-center text-[30px] font-medium leading-[120%] text-[#432261] lg:w-[870px] lg:text-[64px]">
             Turn your idea into a marvelous illustration
           </h1>
           <div className="flex flex-col items-center gap-[57px]">
-            <div className="flex justify-between w-[200px]">
-              <img src="square.svg" alt="" className="w-[30px] h-[30px]" />
+            <div className="flex w-[200px] justify-between">
+              <img src="square.svg" alt="" className="h-[30px] w-[30px]" />
               <img
                 src="green-circle.svg"
                 alt=""
-                className="w-[30px] h-[30px]"
+                className="h-[30px] w-[30px]"
               />
             </div>
-            <img src="triangle.svg" alt="" className="w-[30px] h-[30px]" />
+            <img src="triangle.svg" alt="" className="h-[30px] w-[30px]" />
           </div>
         </div>
       </section>
 
       <section className=" bg-gradient-to-t from-[#744BFF] to-[#9A89FF] ">
-        <div className="flex items-center justify-center flex-col text-white">
+        <div className="flex flex-col items-center justify-center text-white">
           <h1 className="text-lg font-light">Hello, I am</h1>
-          <h1 className="text-2xl mb-[18px]">Muhammad Bilal Arve</h1>
+          <h1 className="mb-[18px] text-2xl">Muhammad Bilal Arve</h1>
           <p className="px-[26px] text-center">
             I am an illustrator and i have 4 years experience. I really love
             turn idea into a marvelous illustration and i love exploring
             illustration style and colours combination
           </p>
         </div>
-        <div className="flex items-center justify-center mt-[48px]">
-          <div className="flex flex-col gap-[24px]  items-start">
-            <div className="flex items-center gap-[24px] justify-center">
+        <div className="mt-[48px] flex items-center justify-center">
+          <div className="flex flex-col items-start  gap-[24px]">
+            <div className="flex items-center justify-center gap-[24px]">
               <div>
                 <img src="work.svg" alt="" />
               </div>
               <div className="text-white">
                 <p>Work Experience</p>
-                <h4 className="text-[24px] font-semibold -mt-2">4 Years</h4>
+                <h4 className="-mt-2 text-[24px] font-semibold">4 Years</h4>
               </div>
             </div>
-            <div className="flex items-center gap-[24px] justify-center">
+            <div className="flex items-center justify-center gap-[24px]">
               <div>
                 <img src="project.svg" alt="" />
               </div>
               <div className="text-white">
                 <p>Project Completed</p>
-                <h4 className="text-[24px] font-semibold -mt-2">400+</h4>
+                <h4 className="-mt-2 text-[24px] font-semibold">400+</h4>
               </div>
             </div>
-            <div className="flex items-center gap-[24px] justify-center">
+            <div className="flex items-center justify-center gap-[24px]">
               <div>
                 <img src="email.svg" alt="" />
               </div>
               <div className="text-white">
                 <p>Contact</p>
-                <h4 className="text-[24px] font-semibold -mt-2">
+                <h4 className="-mt-2 text-[24px] font-semibold">
                   bilalarve@gmail.com
                 </h4>
               </div>
@@ -81,22 +81,30 @@ export default function backup() {
           </div>
         </div>
       </section>
-      <div className="grid grid-rows-2 grid-cols-1">
-        <div className="row-start-1 row-span-1   bg-gradient-to-t from-[#744BFF] to-[#9A89FF] w-full" />
-        <div className="bg-white h-[72px] row-start-2 row-span-1" />
-        <div className="row-span-2 row-start-1 w-[257px] h-[247px] ">
-          <img src="bilal.png" alt="" />
+
+      <div className="grid grid-cols-1 grid-rows-2">
+        <div className="col-start-1 col-end-2 row-start-1 row-end-2 h-40 w-full bg-purple-500"></div>
+        <div className="col-start-1 col-end-2 row-start-2 row-end-3 h-40 w-full bg-white"></div>
+
+        <div className="col-start-1 col-end-2 row-start-1 row-end-3 h-[248px] w-full">
+          <img
+            className="h-full w-full object-contain"
+            src="bilal.png"
+            alt=""
+          />
         </div>
       </div>
 
+      {/* <img src="bilal.png" alt="" /> */}
+
       <section>
-        <h1 className="font-bold text-[22px] text-[#432261] text-center">
+        <h1 className="text-center text-[22px] font-bold text-[#432261]">
           Portfolio
         </h1>
-        <p className="text-[14px] text-[#564795] text-center">
+        <p className="text-center text-[14px] text-[#564795]">
           Here some of my work that i made
         </p>
-        <div className="grid grid-cols-3 grid-rows-2 gap-[14px] mt-[24px]">
+        <div className="mt-[24px] grid grid-cols-3 grid-rows-2 gap-[14px]">
           <img src="nigga-singing.svg" alt="" />
           <img src="nigga-singing.svg" alt="" />
           <img src="nigga-singing.svg" alt="" />
@@ -104,11 +112,11 @@ export default function backup() {
           <img src="nigga-singing.svg" alt="" />
           <img src="nigga-singing.svg" alt="" />
         </div>
-        <div className="flex items-center justify-center mt-[32px]">
-          <button className="bg-gradient-to-tr from-[#744BFF] to-[#A69DFF] rounded-full">
+        <div className="mt-[32px] flex items-center justify-center">
+          <button className="rounded-full bg-gradient-to-tr from-[#744BFF] to-[#A69DFF]">
             <a
               href="#"
-              className="py-[12px] px-[20px] block text-[14px] text-white font-light"
+              className="block py-[12px] px-[20px] text-[14px] font-light text-white"
             >
               Click here for more
             </a>
